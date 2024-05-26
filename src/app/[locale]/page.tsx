6195a72a7e9d74b00a196ca685654e62f90cd8ea
Ariful islam <arifulbgt4@gmail.com>
2024-05-26 14:15:06 +0600

    feat: issue search

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,27 @@
 import PublishedIssueItem from "~/components/PublishedIssueItem/PublishedIssueItem";
 import { getIssues } from "./action";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
+import { Search } from "lucide-react";
 
 export default async function Home() {
   const issues = await getIssues();
   return (
     <div className="container">
-      {issues.map((issue) => (
-        <PublishedIssueItem key={issue.id} {...issue} />
-      ))}
+      <div className="mt-3 flex">
+        <div className="flex w-[70%] flex-col pr-3">
+          <div className="mb-3 flex  w-full items-center space-x-2">
+            <Input type="search" placeholder="Search..." />
+            <Button size="icon" variant="outline">
+              <Search />
+            </Button>
+          </div>
+          {issues.map((issue) => (
+            <PublishedIssueItem key={issue.id} {...issue} />
+          ))}
+        </div>
+        <div className="flex w-[30%] p-3">card</div>
+      </div>
     </div>
   );
 }
