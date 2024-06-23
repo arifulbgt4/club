837b79167eb0ecdcf94c68e94543f5f1d0d0631a
Ariful islam <arifulbgt4@gmail.com>
2024-06-23 17:49:02 +0600

    set last card primary

--- src/app/api/v1/stripe/remove/route.ts
+++ src/app/api/v1/stripe/remove/route.ts
@@ -4,11 +4,25 @@ import { validateRequest } from "~/server/auth";
 export async function POST(req: Request) {
   const body = await req.json();
   try {
-    const { session } = await validateRequest();
+    const { session, user } = await validateRequest();
     if (!session) {
       return new Response("Unauthorized", { status: 401 });
     }
+
     await stripe.paymentMethods.detach(body?.paymentMethodId);
+
+    const paymentMethods = await stripe.paymentMethods.list({
+      customer: user.stripeCustomerId,
+      type: "card",
+    });
+
+    if (!!paymentMethods?.data?.length) {
+      await stripe.customers.update(user?.stripeCustomerId, {
+        invoice_settings: {
+          default_payment_method: paymentMethods?.data[0].id,
+        },
+      });
+    }
     return new Response("success", { status: 200 });
   } catch (error) {
     return new Response("Internal Server Error", { status: 500 });
