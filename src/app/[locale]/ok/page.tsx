09a687a4f275d400894e22944a544b4f04cf5e6e
Ariful islam <arifulbgt4@gmail.com>
2024-07-04 12:36:03 +0600

    fix: auth design

--- src/app/[locale]/ok/page.tsx
+++ src/app/[locale]/ok/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import Icons from "~/components/shared/icons";
 
 const OkPage = () => {
   const router = useRouter();
@@ -15,7 +16,11 @@ const OkPage = () => {
       }
     }
   }, []);
-  return null;
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <Icons.spinner className=" h-9 w-9 animate-spin" />
+    </div>
+  );
 };
 
 export default OkPage;
