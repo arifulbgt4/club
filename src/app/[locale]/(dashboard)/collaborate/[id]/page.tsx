0e857c3a0ec7524d239c3dd65ccd3ef70968c414
Ariful islam <arifulbgt4@gmail.com>
2024-07-26 20:06:17 +0600

    refactor

---
+++ src/app/[locale]/(dashboard)/collaborate/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default function CollaborateDetailsPage({
+  params: { id },
+  //   searchParams: { page },
+}: {
+  params: { id: string };
+  searchParams: { page: string };
+}) {
+  return <div>CollaborateDetailsPage id={id}</div>;
+}
