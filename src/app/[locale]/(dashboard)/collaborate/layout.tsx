b3405633270e73fe0579ce25693216d6ceafc385
Ariful islam <arifulbgt4@gmail.com>
2024-07-26 19:54:51 +0600

    refactor

---
+++ src/app/[locale]/(dashboard)/collaborate/layout.tsx
@@ -0,0 +1,12 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col flex-nowrap gap-4 lg:flex-row lg:gap-9">
+      <div className="absolute -left-[300px] flex flex-col rounded-tr-md border-r lg:relative lg:left-0 lg:w-[300px]">
+        <ul className="h-[calc(100vh-222px)] overflow-auto rounded-md pr-3">
+          sidebar
+        </ul>
+      </div>
+      <div className="w-full lg:w-[calc(100%-300px)]">{children}</div>
+    </div>
+  );
+}
