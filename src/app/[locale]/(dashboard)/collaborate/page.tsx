8a13a20862be8aee2d60013da9aea202307387f1
Ariful islam <arifulbgt4@gmail.com>
2024-07-29 16:00:34 +0600

    refactor

--- src/app/[locale]/(dashboard)/collaborate/page.tsx
+++ src/app/[locale]/(dashboard)/collaborate/page.tsx
@@ -1,3 +1,24 @@
 export default function CollaboratePage() {
-  return <div>Collaborate</div>;
+  return (
+    <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-1">
+        <h3 className=" text-xl font-semibold">
+          The collaborator board only for private repository
+        </h3>
+        <p className="text-muted-foreground">
+          If you are ina repository Collaborator then you can see those
+          repository in left side list.
+        </p>
+      </div>
+      <div className="flex flex-col gap-1">
+        <h3 className=" text-xl font-semibold">
+          You are able to see list of issues in a repositorys assigned
+        </h3>
+        <p className="text-muted-foreground">
+          If you are ina repository Collaborator then you can see those
+          repository in left side list.
+        </p>
+      </div>
+    </div>
+  );
 }
