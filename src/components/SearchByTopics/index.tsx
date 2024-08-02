ffdb495b5eeaaaf55b63a48cd037888029e33ff2
Ariful islam <arifulbgt4@gmail.com>
2024-08-02 20:41:38 +0600

    refactor

--- src/components/SearchByTopics/index.tsx
+++ src/components/SearchByTopics/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { type FC } from "react";
 import type { SearchByTopicsProps } from "./Types";
-
 import { useRouter } from "next/navigation";
 import { cn } from "~/lib/utils";
 import SearchTopics from "../SearchTopics";
@@ -12,6 +11,14 @@ const SearchByTopics: FC<SearchByTopicsProps> = ({
 }) => {
   const router = useRouter();
 
+  function onChange(v: string[]) {
+    if (!!v?.length) {
+      router.push(`/?topics=${v.join(",")}`);
+      return;
+    }
+    router.push("/");
+  }
+
   return (
     <div
       className={cn(
@@ -19,16 +26,7 @@ const SearchByTopics: FC<SearchByTopicsProps> = ({
         "flex min-h-12 w-full items-center gap-2 rounded-lg border px-2 py-1"
       )}
     >
-      <SearchTopics
-        value={params}
-        onChange={(v) => {
-          if (!!v?.length) {
-            router.push(`/?topics=${v.join(",")}`);
-            return;
-          }
-          router.push("/");
-        }}
-      />
+      <SearchTopics value={params} onChange={onChange} />
     </div>
   );
 };
