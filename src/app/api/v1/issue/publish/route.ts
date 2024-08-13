feb4075d13a5dbec39c4b174dd647410d16b6eaa
Ariful islam <arifulbgt4@gmail.com>
2024-08-13 17:51:04 +0600

    refactor

--- src/app/api/v1/issue/publish/route.ts
+++ src/app/api/v1/issue/publish/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: Request) {
       const reqUser = await db.user.findUnique({
         where: { githubId: String(body?.collaborator?.id) },
       });
-      isQueue = reqUser?.available as boolean;
+      isQueue = !reqUser?.available;
     }
 
     const octo = await octokit();
